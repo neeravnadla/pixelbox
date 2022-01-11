94f886c718fe20073955d1c8ad3ec9782ca68df7
neenadla <neeravnadla@gmail.com>
2022-01-11 13:56:23 +0530

    function updated

--- pixel.js
+++ pixel.js
@@ -1,79 +1,51 @@
 "use strict";
 
-document.querySelector("body").addEventListener("load", pageoad());
+document.querySelector("body").addEventListener("load", pageLoad());
 
-function pageoad() {
+function pageLoad() {
   document.querySelectorAll(".in-numb").forEach((e) => {
-    // intial random value
     e.value = Math.floor(Math.random() * 21);
-    e.addEventListener("input", createPixel);
+    e.addEventListener("input", generatePixelbox);
   });
-
-  createPixel();
+  generatePixelbox();
 }
 
-function createPixel() {
+function generatePixelbox() {
   let pixelOne = parseInt(document.querySelector("#pixel-1").value);
   let pixelTwo = parseInt(document.querySelector("#pixel-2").value);
+  let oldBoxes = document.querySelectorAll(".smallbox");
+  oldBoxes.forEach((e) => e.remove());
   if (isNaN(pixelOne) === true) pixelOne = 0;
   if (isNaN(pixelTwo) === true) pixelTwo = 0;
-  if (pixelOne >= 0 && pixelTwo >= 0) {
-    let totalPixel = perfectPixel(pixelOne + pixelTwo);
-    insertBox(totalPixel);
-    return createboxes(totalPixel, pixelOne, pixelTwo);
-  }
+  if (pixelOne >= 0 && pixelTwo >= 0)
+    newBoxes(perfectPixel(pixelOne + pixelTwo), pixelOne, pixelTwo);
 }
 
-function insertBox(bb) {
-  document.querySelectorAll(".smallbox").forEach((e) => {
-    e.remove();
-  });
-
-  let boxArray = [];
-  boxArray.length = bb;
-  boxArray.fill(0);
-
-  boxArray.forEach((e) => {
-    let div = document.createElement("div");
-    div.className = "smallbox";
-    document.querySelector(".pixelbox").appendChild(div);
-  });
-
-  document.querySelectorAll(".smallbox").forEach((e) => {
-    let boxsize = 250 / Math.sqrt(bb);
-    e.style.width = boxsize + "px";
-    e.style.height = boxsize + "px";
-  });
-}
-
-function createboxes(t, p1, p2) {
+function newBoxes(t, p1, p2) {
   let boxes = [];
   boxes.length = parseInt(t);
   boxes.fill(0);
 
-  for (let i = 0; i < p1; i++) {
-    boxes[i] = 1;
-  }
-
-  for (let i = p1; i < p1 + p2; i++) {
-    boxes[i] = 2;
+  let i = 0;
+  while (i < p1 + p2) {
+    if (i < p1) boxes[i] = 1;
+    else boxes[i] = 2;
+    i++;
   }
 
   let shuffledboxes = shuffleArray(boxes);
-
-  let boxid = document.querySelectorAll(".smallbox");
+  let boxsize = 250 / Math.sqrt(t);
 
   for (let i = 0; i < shuffledboxes.length; i++) {
-    if (shuffledboxes[i] === 1) {
-      boxid[i].style.backgroundColor = "var(--p1-color)";
-    }
-    if (shuffledboxes[i] === 2) {
-      boxid[i].style.backgroundColor = "var(--p2-color)";
-    }
+    let div = document.createElement("div");
+    div.className = "smallbox";
+    div.style.width = boxsize + "px";
+    div.style.height = boxsize + "px";
+    document.querySelector(".pixelbox").appendChild(div);
+    if (shuffledboxes[i] === 1) div.style.backgroundColor = "var(--p1-color)";
+    if (shuffledboxes[i] === 2) div.style.backgroundColor = "var(--p2-color)";
   }
 }
-
-// functions
 
 function perfectPixel(N) {
   if (Math.sqrt(N) - Math.floor(Math.sqrt(N)) != 0) {
@@ -86,11 +58,9 @@ function perfectPixel(N) {
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
-
     var temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
-
   return array;
 }
