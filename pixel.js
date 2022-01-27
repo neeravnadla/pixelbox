f7e78e784cb8e666fcd933f6af68fb8966ce9f52
neenadla <neeravnadla@gmail.com>
2022-01-27 19:50:30 +0530

    added grid layout

--- pixel.js
+++ pixel.js
@@ -6,47 +6,38 @@ document.querySelector(".pixelbox").addEventListener("click", pageLoad);
 function pageLoad() {
   document.querySelectorAll(".in-numb").forEach((e) => {
     e.value = Math.floor(Math.random() * 21);
-    e.addEventListener("input", generatePixelbox);
+    e.addEventListener("keyup", generatePixelbox);
   });
   generatePixelbox();
 }
 
-
-
 function generatePixelbox() {
+  document.querySelectorAll(".smallbox").forEach((e) => e.remove());
+
   let pixelOne = parseInt(document.querySelector("#pixel-1").value);
   let pixelTwo = parseInt(document.querySelector("#pixel-2").value);
-  let oldBoxes = document.querySelectorAll(".smallbox");
-  oldBoxes.forEach((e) => e.remove());
-  if (isNaN(pixelOne) === true) pixelOne = 0;
-  if (isNaN(pixelTwo) === true) pixelTwo = 0;
-  if (pixelOne >= 0 && pixelTwo >= 0)
-    newBoxes(perfectPixel(pixelOne + pixelTwo), pixelOne, pixelTwo);
+
+  // check input is number.
+  if (isNaN(pixelOne)) pixelOne = 0;
+  if (isNaN(pixelTwo)) pixelTwo = 0;
+  if (pixelOne >= 0 && pixelTwo >= 0) {
+    return newBoxes(perfectPixel(pixelOne + pixelTwo), pixelOne, pixelTwo);
+  }
 }
 
 function newBoxes(t, p1, p2) {
-  let boxes = [];
-  boxes.length = parseInt(t);
-  boxes.fill(0);
+    
+  let color = ["#CA965C", "#EEC373", "#F4DFBA"]; 
 
-  let i = 0;
-  while (i < p1 + p2) {
-    if (i < p1) boxes[i] = 1;
-    else boxes[i] = 2;
-    i++;
-  }
+  // set grid row and column.
+  document.querySelector(".pixelbox").style.setProperty("--grid", Math.sqrt(t));
 
-  let shuffledboxes = shuffleArray(boxes);
-  let boxsize = 250 / Math.sqrt(t);
-
-  for (let i = 0; i < shuffledboxes.length; i++) {
+  // create box.
+  for (let i = 0; i < t; i++) {
     let div = document.createElement("div");
     div.className = "smallbox";
-    div.style.width = boxsize + "px";
-    div.style.height = boxsize + "px";
     document.querySelector(".pixelbox").appendChild(div);
-    if (shuffledboxes[i] === 1) div.style.backgroundColor = "var(--p1-color)";
-    if (shuffledboxes[i] === 2) div.style.backgroundColor = "var(--p2-color)";
+    div.style.backgroundColor = color[Math.floor(Math.random() * 3)];
   }
 }
 
@@ -56,14 +47,4 @@ function perfectPixel(N) {
     return nextN * nextN;
   }
   return N;
-}
-
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
 }
